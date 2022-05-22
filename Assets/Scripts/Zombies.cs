ff0b08ffaddce5c545c5dd96dc57a5bbca28f9ae
noodulcode <gcab012203@coderacademy.edu.au>
2022-05-22 13:07:01 +1000

    create enemy movement script

---
+++ Assets/Scripts/Zombies.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombies : MonoBehaviour
+{
+    [HideInInspector] // needs to be accessible not in the inspector tab
+    public float speed;
+    private Rigidbody2D myBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+
+        speed = 7;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        myBody.velocity = new Vector2(speed, myBody.velocity.y); // (velocity x, and velocity y) only use speed on x
+    }
+
+
+
+
+} // class
